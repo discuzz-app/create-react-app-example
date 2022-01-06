773273957c5991fc5a02c0633c78abb543c69539
Martin Pham <tuyentq2009@gmail.com>
2022-01-06 22:52:08 +0100

    helloworld

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# Getting Started with Create React App
+# Create React App integration with Discuzz
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a [Create React App](https://github.com/facebook/create-react-app) example with integration with [Discuzz](https://github.com/discuzz-app/discuzz).
+
+## Demo
+You can check deployed site here [https://discuzz-app.github.io/create-react-app-example/](https://discuzz-app.github.io/create-react-app-example/).
+
 
 ## Available Scripts
 
@@ -38,9 +42,3 @@ If you aren’t satisfied with the build tool and configuration choices, you can
 Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
 
 You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Discuzz, Config, Theme, prefersDarkMode } from '@discuzz/discuzz'
+
+const LocaleProviderEn = lazy(() => import('@discuzz/locale-en'))
+
+const ComposerPlaintext = lazy(() => import('@discuzz/composer-plaintext'))
+
+const ViewerPlaintext = lazy(() => import('@discuzz/viewer-plaintext'))
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<span>...</span>}>
+        <Discuzz
+          url={global.location.href}
+          service={{
+            apiKey: "AIzaSyDm837cbdbvkrAdYL9TAqUF3iML6UvZXk4",
+            authDomain: "fire-talk-88.firebaseapp.com",
+            projectId: "fire-talk-88",
+            storageBucket: "fire-talk-88.appspot.com",
+            messagingSenderId: "719566664522",
+            appId: "1:719566664522:web:e1a9d26be22387e55b47b3"
+          }}
+          theme="light"
+          auths={['google', 'apple', 'facebook', 'github', 'twitter', 'microsoft', 'yahoo']}
+          config={{
+            composer: ComposerPlaintext,
+            viewer: ViewerPlaintext
+          }}
+          locale={LocaleProviderEn}
+        />
+      </Suspense>
     </div>
   );
 }
