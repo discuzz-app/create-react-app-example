b4b62304daec75aec31c829dbf88dc16d751863e
Martin Pham <tuyentq2009@gmail.com>
2022-01-07 22:28:18 +0100

    helloworld

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
 import { lazy, Suspense } from 'react';
-import { Discuzz } from '@discuzz/discuzz'
+import { Discuzz, loadService } from '@discuzz/discuzz'
 
 const LocaleProviderEn = lazy(() => import('@discuzz/locale-en'))
-
-const ComposerPlaintext = lazy(() => import('@discuzz/composer-plaintext'))
-
-const ViewerPlaintext = lazy(() => import('@discuzz/viewer-plaintext'))
+const AuthFirebase = loadService(() => import('@discuzz/auth-firebase'))
+const DataFirestore = loadService(() => import('@discuzz/data-firestore'))
 
 function App() {
+  
   return (
     <div>
       <Suspense fallback={<span>...</span>}>
         <Discuzz
           url={global.location.href}
           service={{
-            apiKey: "AIzaSyDm837cbdbvkrAdYL9TAqUF3iML6UvZXk4",
-            authDomain: "fire-talk-88.firebaseapp.com",
-            projectId: "fire-talk-88",
-            storageBucket: "fire-talk-88.appspot.com",
-            messagingSenderId: "719566664522",
-            appId: "1:719566664522:web:e1a9d26be22387e55b47b3"
+            auth: AuthFirebase,
+            data: DataFirestore,
+            config: {
+              apiKey: "AIzaSyDm837cbdbvkrAdYL9TAqUF3iML6UvZXk4",
+              authDomain: "fire-talk-88.firebaseapp.com",
+              projectId: "fire-talk-88",
+              storageBucket: "fire-talk-88.appspot.com",
+              messagingSenderId: "719566664522",
+              appId: "1:719566664522:web:e1a9d26be22387e55b47b3"
+            }
           }}
           theme="light"
           auths={['google', 'apple', 'facebook', 'github', 'twitter', 'microsoft', 'yahoo']}
-          config={{
-            composer: ComposerPlaintext,
-            viewer: ViewerPlaintext
-          }}
           locale={LocaleProviderEn}
         />
       </Suspense>
